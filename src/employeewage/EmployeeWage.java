71d2b048ac9f9ba91ca833a64b91bf856e6879ff
DivyaMundhada <divyamundada9@gmail.com>
2021-06-30 13:41:30 +0530

    Refactor the Code to write Class Method to compute Emp Wage

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -4,23 +4,13 @@ public class EmployeeWage {
 
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
-
-	public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		this.company = company;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
-	}
-
-	public void computeEmpWage() {
+	public static int computeEmpWage() {
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -36,20 +26,12 @@ public class EmployeeWage {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
 		}
-		totalEmpWage = totalEmpHrs * empRatePerHour;
-	}
-
-	@Override
-	public String toString() {
-		return "Total Employee Wage for Company: " + company + " is: " + totalEmpWage;
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
-		EmployeeWage reliance = new EmployeeWage("Reliance", 10, 4, 20);
-		dMart.computeEmpWage();
-		System.out.println(dMart);
-		reliance.computeEmpWage();
-		System.out.println(reliance);
+		computeEmpWage();
 	}
 }
