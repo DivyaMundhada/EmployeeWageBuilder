38052a8950d7445a6eede5f0de0e8bc0896337ed
DivyaMundhada <divyamundada9@gmail.com>
2021-07-02 10:29:30 +0530

    Ability to manage Employee Wage of Multiple Companies.

---
+++ src/employeewage/CompanyEmpWage.java
@@ -0,0 +1,25 @@
+package employeewage;
+
+public class CompanyEmpWage {
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpWage;
+
+	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+		this.numOfWorkingDays = numOfWorkingDays;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Employee Wage for Company: " + company + " is: " + totalEmpWage;
+	}
+}
